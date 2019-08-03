10c0e901d12a8d4b3dd249a4fb59de57098eea80
dheat <A582ra!wsd>
2019-08-03 03:03:30 +0100

    =Modified all batch files to correctly reference Python scripts in upper-directory

--- source/batch_files/file_predictor_leaveout.cmd
+++ source/batch_files/file_predictor_leaveout.cmd
@@ -17,36 +17,36 @@ if %directory% == allmatfiles set measures=jointAngle
 @echo.
 @echo ---------- Running 'ext_raw_measures.py...' ----------
 @echo.
-python ext_raw_measures.py %directory% %file_name% %file_type%
+python ..\ext_raw_measures.py %directory% %file_name% %file_type%
 
 
 @echo.
 @echo ---------- Running 'comp_stat_vals.py...' ----------
 @echo.
-if %directory% == NSAA python comp_stat_vals.py %directory% AD %file_name% --split_size=1
+if %directory% == NSAA python ..\comp_stat_vals.py %directory% AD %file_name% --split_size=1
 
 
 @echo.
 @echo ---------- Running 'ft_sel_red.py...' ----------
 @echo.
-if %directory% == NSAA python ft_sel_red.py %directory% AD %file_name% pca --num_features% %=%30 --no_normalize
+if %directory% == NSAA python ..\ft_sel_red.py %directory% AD %file_name% pca --num_features% %=%30 --no_normalize
 
 
-python rnn.py %directory% position all dhc --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
-python rnn.py %directory% position all overall --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
-python rnn.py %directory% position all acts --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
-python rnn.py %directory% sensorMagneticField all dhc --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
-python rnn.py %directory% sensorMagneticField all overall --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
-python rnn.py %directory% sensorMagneticField all acts --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
-python rnn.py %directory% jointAngle all dhc --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
-python rnn.py %directory% jointAngle all overall --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
-python rnn.py %directory% jointAngle all acts --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
-python rnn.py %directory% AD all dhc --seq_len=10 --seq_overlap=0.9 --epochs=100 --leave_out=%file_name%
-python rnn.py %directory% AD all overall --seq_len=10 --seq_overlap=0.9 --epochs=100 --leave_out=%file_name%
-python rnn.py %directory% AD all acts --seq_len=10 --seq_overlap=0.9 --epochs=100 --leave_out=%file_name%
+python ..\rnn.py %directory% position all dhc --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
+python ..\rnn.py %directory% position all overall --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
+python ..\rnn.py %directory% position all acts --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
+python ..\rnn.py %directory% sensorMagneticField all dhc --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
+python ..\rnn.py %directory% sensorMagneticField all overall --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
+python ..\rnn.py %directory% sensorMagneticField all acts --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
+python ..\rnn.py %directory% jointAngle all dhc --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
+python ..\rnn.py %directory% jointAngle all overall --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
+python ..\rnn.py %directory% jointAngle all acts --seq_len=600 --seq_overlap=0.9 --discard_prop=0.9 --epochs=20 --leave_out=%file_name%
+python ..\rnn.py %directory% AD all dhc --seq_len=10 --seq_overlap=0.9 --epochs=100 --leave_out=%file_name%
+python ..\rnn.py %directory% AD all overall --seq_len=10 --seq_overlap=0.9 --epochs=100 --leave_out=%file_name%
+python ..\rnn.py %directory% AD all acts --seq_len=10 --seq_overlap=0.9 --epochs=100 --leave_out=%file_name%
 
 
 @echo.
 @echo ---------- Running 'model_predictor.py...' ----------
 @echo.
-python model_predictor.py %directory% %measures% %file_name%
+python ..\model_predictor.py %directory% %measures% %file_name%
